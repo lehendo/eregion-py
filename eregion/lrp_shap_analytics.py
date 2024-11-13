1447eb3d9ac301eaa1143a3c38cc5b67375ab6f4
lehendo <arj0jeechat@gmail.com>
2024-11-13 17:34:50 -0600

    update prev init

--- eregion/lrp_shap_analytics.py
+++ eregion/lrp_shap_analytics.py
@@ -5,15 +5,19 @@ import tensorflow as tf
 from typing import List, Dict
 from torch import nn
 
-class LrpShapAnalytics:
+
+class NeuralNetworkAnalytics:
     def __init__(self, model, data_loader=None, framework='pytorch'):
         """
-        Initialize with a model and data loader.
-"""
+        Initialize a model + data loader.
+        """
         self.model = model
         self.data_loader = data_loader
         self.framework = framework
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'  # For PyTorch
 
-        if self.framework == 'tensorflow' and isinstance(model, tf.keras.Model):
-            self.model = model  # TensorFlow model initialization
+        if self.framework == 'pytorch':
+            self.model.to(self.device)
+            self.model.eval()
+
+    
