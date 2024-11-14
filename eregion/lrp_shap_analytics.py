2dff51d22295647ae89d5addb13cfd499b0bef54
lehendo <arj0jeechat@gmail.com>
2024-11-13 18:09:42 -0600

    zennit for pytorch

--- eregion/lrp_shap_analytics.py
+++ eregion/lrp_shap_analytics.py
@@ -5,6 +5,11 @@ import tensorflow as tf
 from typing import List, Dict
 from torch import nn
 
+from zennit.torchvision import ResNetCanonizer # not needed rn
+from zennit.composites import EpsilonPlusFlat
+from zennit.rules import Epsilon
+
+import innvestigate
 
 class LrpShapAnalytics:
     def __init__(self, model, data_loader=None, framework='pytorch'):
@@ -20,5 +25,53 @@ class LrpShapAnalytics:
             self.model.to(self.device)
             self.model.eval()
 
+    def compute_lrp_relevance(self) -> Dict[int, np.ndarray]:
+        """
+        Compute/aggregate LRP relevance scores for each neuron in each layer.
+        """
+        if self.framework == 'pytorch':
+            return self.compute_lrp_pytorch()
+        elif self.framework == 'tensorflow':
+            return self.compute_lrp_tensorflow()
 
+    def compute_lrp_pytorch(self) -> Dict[int, np.ndarray]:
+        """
+        Compute LRP relevance for PyTorch models using Zennit.
+        """
+        relevance_scores = {}
 
+        for inputs, _ in self.data_loader:
+            inputs = inputs.to(self.device)
+
+            relevance_per_layer = self.run_lrp_pytorch(inputs)
+
+            for layer_idx, rel in relevance_per_layer.items():
+                if layer_idx not in relevance_scores:
+                    relevance_scores[layer_idx] = []
+                relevance_scores[layer_idx].append(rel.cpu().detach().numpy())
+
+        for layer_idx, rel_list in relevance_scores.items():
+            relevance_scores[layer_idx] = np.mean(rel_list, axis=0)
+
+        return relevance_scores
+
+    def run_lrp_pytorch(self, inputs):
+        """
+        Run LRP on the model for given inputs using Zennit.
+        """
+        relevance_per_layer = {}
+
+        composite = EpsilonPlusFlat(Epsilon())  # Standard LRP rule for stability
+
+        with composite.context(self.model) as modified_model:
+            # get outputs
+            outputs = modified_model(inputs)
+            # Propagate relevance
+            relevance = modified_model.relprop(outputs)
+
+        # Gathering relevance scores every layer
+        for i, layer in enumerate(self.model.children()):
+            if hasattr(layer, 'weight'):
+                relevance_per_layer[i] = relevance[layer].detach()
+
+        return relevance_per_layer
